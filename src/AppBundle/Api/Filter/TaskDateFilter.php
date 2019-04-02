b1959d668e08457eb590c977a64153aa18e16769
Alexandre Segura <mex.zktk@gmail.com>
2019-04-02 08:43:56 +0200

    WIP TaskDateFilter

--- src/AppBundle/Api/Filter/TaskDateFilter.php
+++ src/AppBundle/Api/Filter/TaskDateFilter.php
@@ -11,21 +11,6 @@ use Psr\Log\LoggerInterface;
 
 final class TaskDateFilter extends AbstractContextAwareFilter
 {
-    // private $property;
-
-    // public function __construct(
-    //     ManagerRegistry $managerRegistry,
-    //     $requestStack = null,
-    //     LoggerInterface $logger = null,
-    //     array $properties = null,
-    //     string $property = null)
-    // {
-    //     parent::__construct($managerRegistry, $requestStack, $logger, $properties);
-
-    //     // $this->tokenStorage = $tokenStorage;
-    //     $this->property = $property;
-    // }
-
     protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
         // Only works on Task class
@@ -33,26 +18,10 @@ final class TaskDateFilter extends AbstractContextAwareFilter
             return;
         }
 
-        // var_dump($property);
-        // var_dump($this->isPropertyEnabled($property, $resourceClass));
-        // var_dump($this->isPropertyMapped($property, $resourceClass));
-        // // var_dump($this->property);
-        // var_dump($resourceClass);
-
         // otherwise filter is applied to order and page as well
         if (!$this->isPropertyEnabled($property, $resourceClass)) {
             return;
         }
-
-
-
-        // if (!$this->isPropertyMapped($property, $resourceClass) && $this->property) {
-
-        //     $property = $this->property;
-        // }
-
-        // ->andWhere(':date >= DATE(t.doneAfter)')
-        // ->andWhere(':date <= DATE(t.doneBefore)')
 
         $afterParameterName = $queryNameGenerator->generateParameterName('doneAfter');
         $beforeParameterName = $queryNameGenerator->generateParameterName('doneBefore');
@@ -62,8 +31,6 @@ final class TaskDateFilter extends AbstractContextAwareFilter
             ->andWhere(sprintf(':%s <= DATE(o.%s)', $beforeParameterName, 'doneBefore'))
             ->setParameter($afterParameterName, $value)
             ->setParameter($beforeParameterName, $value);
-
-        // var_dump($queryBuilder->getQuery()->getSQL());
     }
 
     public function getDescription(string $resourceClass): array
