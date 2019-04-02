27607fa0e473999b8d3d17699d9390a5a4bccdf8
Alexandre Segura <mex.zktk@gmail.com>
2019-04-02 08:20:13 +0200

    Refactor TaskRepository.

--- src/AppBundle/Entity/TaskRepository.php
+++ src/AppBundle/Entity/TaskRepository.php
@@ -8,32 +8,33 @@ use Doctrine\ORM\Query\Expr;
 
 class TaskRepository extends EntityRepository
 {
-    public function findByDate(\DateTime $date)
+    private function createDateAwareQueryBuilder(\DateTime $date)
     {
         return $this->createQueryBuilder('t')
             ->andWhere(':date >= DATE(t.doneAfter)')
             ->andWhere(':date <= DATE(t.doneBefore)')
-            ->setParameter('date', $date->format('Y-m-d'))
+            ->setParameter('date', $date->format('Y-m-d'));
+    }
+
+    public function findByDate(\DateTime $date)
+    {
+        return $this->createDateAwareQueryBuilder($date)
             ->getQuery()
             ->getResult();
     }
 
     public function findUnassigned(\DateTime $date)
     {
-        return $this->createQueryBuilder('t')
+        return $this->createDateAwareQueryBuilder($date)
             ->andWhere('t.assignedTo IS NULL')
-            ->andWhere('DATE(t.doneBefore) = :date')
-            ->setParameter('date', $date->format('Y-m-d'))
             ->getQuery()
             ->getResult();
     }
 
     public function findAssigned(\DateTime $date)
     {
-        return $this->createQueryBuilder('t')
+        return $this->createDateAwareQueryBuilder($date)
             ->andWhere('t.assignedTo IS NOT NULL')
-            ->andWhere('DATE(t.doneBefore) = :date')
-            ->setParameter('date', $date->format('Y-m-d'))
             ->getQuery()
             ->getResult();
     }
